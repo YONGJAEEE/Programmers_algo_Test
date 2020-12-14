75d7c8f3f936d3d4c576534e44e2bfa4e41033aa
YONGJAEEE <56459713+YONGJAEEE@users.noreply.github.com>
2020-12-14 15:12:23 +0900

    x만큼 간격이 있는 문제 해결

--- Kotlin/x만큼간격이있는.kt
+++ Kotlin/x만큼간격이있는.kt
@@ -1,35 +1,10 @@
 class Solution {
-    fun solution(answers: IntArray): ArrayList<Int> {
-    val answer = ArrayList<Int>()
-    val score : ArrayList<Int> = arrayListOf(0,0,0)
-    val sortedList = ArrayList<Int>()
-    val math1 = intArrayOf(1, 2, 3, 4, 5)
-    val math2 = intArrayOf(2, 1, 2, 3, 2, 4, 2, 5)
-    val math3 = intArrayOf(3, 3, 1, 1, 2, 2, 4, 4, 5, 5)
-
-    for (i in answers.indices) {
-        if (answers[i] == math1[i % math1.size]) {
-            score[0]++
-        }
-    }
-    for (i in answers.indices) {
-        if (answers[i] == math2[i % math2.size]) {
-            score[1]++
-        }
-    }
-    for (i in answers.indices) {
-        if (answers[i] == math3[i % math3.size]) {
-            score[2]++
-        }
-    }
-
-    for (i in score) sortedList.add(i)
-
-    sortedList.sort()
-
-    print(score)
-    for (i in score.indices){
-        if(score[i] == sortedList[2]) answer.add(i+1)
+    fun solution(x: Int, n: Int): ArrayList<Long> {
+    var answer = ArrayList<Long>()
+    var temp : Long = 0
+    for (i in 1..n) {
+        temp += x
+        answer.add(temp.toLong())
     }
     return answer
 }
