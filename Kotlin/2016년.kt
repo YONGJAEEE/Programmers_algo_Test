eb5293f439e96fcde10f72e8352d85c85cbfddb6
YONGJAEEE <56459713+YONGJAEEE@users.noreply.github.com>
2020-12-14 14:59:25 +0900

    2016년 문제 해결

--- Kotlin/2016년.kt
+++ Kotlin/2016년.kt
@@ -1,36 +1,19 @@
+import java.util.*
 class Solution {
-    fun solution(answers: IntArray): ArrayList<Int> {
-    val answer = ArrayList<Int>()
-    val score : ArrayList<Int> = arrayListOf(0,0,0)
-    val sortedList = ArrayList<Int>()
-    val math1 = intArrayOf(1, 2, 3, 4, 5)
-    val math2 = intArrayOf(2, 1, 2, 3, 2, 4, 2, 5)
-    val math3 = intArrayOf(3, 3, 1, 1, 2, 2, 4, 4, 5, 5)
-
-    for (i in answers.indices) {
-        if (answers[i] == math1[i % math1.size]) {
-            score[0]++
+    fun solution(a: Int, b: Int): String {
+        val cal = Calendar.getInstance()
+        var answer = ""
+        cal.set(2016,a-1,b)
+        val dayNum = cal.get(Calendar.DAY_OF_WEEK)
+        when(dayNum){
+            1 -> answer = "SUN"
+            2 -> answer = "MON"
+            3 -> answer = "TUE"
+            4 -> answer = "WED"
+            5 -> answer = "THU"
+            6 -> answer = "FRI"
+            7 -> answer = "SAT"
         }
+        return answer
     }
-    for (i in answers.indices) {
-        if (answers[i] == math2[i % math2.size]) {
-            score[1]++
-        }
-    }
-    for (i in answers.indices) {
-        if (answers[i] == math3[i % math3.size]) {
-            score[2]++
-        }
-    }
-
-    for (i in score) sortedList.add(i)
-
-    sortedList.sort()
-
-    print(score)
-    for (i in score.indices){
-        if(score[i] == sortedList[2]) answer.add(i+1)
-    }
-    return answer
-}
 }
