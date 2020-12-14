e25e83d3060571e6ea5532c941b2f58dbf9f4ecf
YONGJAEEE <56459713+YONGJAEEE@users.noreply.github.com>
2020-12-14 15:10:01 +0900

    시저암호 문제 해결

--- Kotlin/시저암호.kt
+++ Kotlin/시저암호.kt
@@ -1,35 +1,29 @@
 class Solution {
-    fun solution(answers: IntArray): ArrayList<Int> {
-    val answer = ArrayList<Int>()
-    val score : ArrayList<Int> = arrayListOf(0,0,0)
-    val sortedList = ArrayList<Int>()
-    val math1 = intArrayOf(1, 2, 3, 4, 5)
-    val math2 = intArrayOf(2, 1, 2, 3, 2, 4, 2, 5)
-    val math3 = intArrayOf(3, 3, 1, 1, 2, 2, 4, 4, 5, 5)
-
-    for (i in answers.indices) {
-        if (answers[i] == math1[i % math1.size]) {
-            score[0]++
-        }
-    }
-    for (i in answers.indices) {
-        if (answers[i] == math2[i % math2.size]) {
-            score[1]++
-        }
-    }
-    for (i in answers.indices) {
-        if (answers[i] == math3[i % math3.size]) {
-            score[2]++
-        }
+fun solution(s: String, n: Int): String {
+    var answer = ""
+    val strList = ArrayList<Char>()
+    for (i in s.indices) {
+        strList.add(s.get(i))
     }
 
-    for (i in score) sortedList.add(i)
-
-    sortedList.sort()
-
-    print(score)
-    for (i in score.indices){
-        if(score[i] == sortedList[2]) answer.add(i+1)
+    for (i in strList.indices) {
+        var strToNum = strList[i].toInt()
+        if (strToNum == 32) {
+            answer += " "
+        } else {
+            if (strToNum in 65..90) {
+                strToNum += n
+                if (strToNum > 90) {
+                    strToNum = 65 + ((strToNum - 91) % 26)
+                }
+            } else {
+                strToNum += n
+                if (strToNum > 122) {
+                    strToNum = 97 + ((strToNum - 123) % 26)
+                }
+            }
+            answer += strToNum.toChar()
+        }
     }
     return answer
 }
